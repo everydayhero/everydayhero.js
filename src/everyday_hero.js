be94be01b05210c7ed6c9cd6327d7b5a267b916e
Tate Johnson <tate@tatey.com>
2011-12-15 14:07:07 +1000

    Public constructor is a dumb function

--- src/everyday_hero.js
+++ src/everyday_hero.js
@@ -3,32 +3,33 @@
     this.resource = resource;
     this.id       = id;
     this.host     = options.host || 'api.everydayhero.com.au';
+    
+    this.topTen = function(query, callback) {
+      this._request('/top_ten/' + query + '/:callback.json?callback=:callback', callback);      
+    }
+
+    this.total = function(callback) {
+      this._request('/total/:callback.json?callback=:callback', callback);
+    }
+
+    this._request = function(path, callback) {
+      var functionName = 'everydayhero_' + Math.floor(Math.random()*1000001);    
+      reqwest({
+        url: this._url(path.replace(/:callback/g, functionName)),
+        type: 'jsonp',
+        jsonpCallback: 'callback',
+        success: callback
+      });
+    }
+
+    this._url = function(path) {
+      return 'http://' + this.host + '/widget/' + this.resource + '/' + this.id + path;
+    }
+    
     return this;
   }
-  
-  EverydayHero.prototype.top_ten = function(query, callback) {
-    this._request('/top_ten/' + query + '/:callback.json?callback=:callback', callback);      
-  }
-  
-  EverydayHero.prototype.total = function(callback) {
-    this._request('/total/:callback.json?callback=:callback', callback);
-  }
-  
-  EverydayHero.prototype._request = function(path, callback) {
-    var functionName = 'everydayhero_' + Math.floor(Math.random()*1000001);    
-    reqwest({
-      url: this._url(path.replace(/:callback/g, functionName)),
-      type: 'jsonp',
-      jsonpCallback: 'callback',
-      success: callback
-    });
-  }
-  
-  EverydayHero.prototype._url = function(path) {
-    return 'http://' + this.host + '/widget/' + this.resource + '/' + this.id + path;
-  }
     
-  window.EverydayHero = function(resource, id, options) {
-    return new EverydayHero(resource, id, options);
+  window.EverydayHero = function() {
+    return EverydayHero.apply(this, arguments);
   }
 })(window);
