aa96a40e2b3ab0f27a3d4434bf94b117bd17cc85
Tate Johnson <tate@tatey.com>
2011-12-15 14:17:08 +1000

    Renamed file

--- src/everydayhero.js
+++ src/everydayhero.js
@@ -1,3 +1,10 @@
+/*!
+  * Everyday Hero JavaScript Widget API Wrapper
+  * https://github.com/everydayhero/everydayhero.js
+  * Copyright © 2011 Everyday Hero Pty. Ltd.
+  * MIT License
+  */
+
 (function(window) {
   var EverydayHero = function(resource, id, options) {
     this.resource = resource;
@@ -5,17 +12,17 @@
     this.host     = options.host || 'api.everydayhero.com.au';
     
     this.topTen = function(query, callback) {
-      this._request('/top_ten/' + query + '/:callback.json?callback=:callback', callback);      
+      this._request('/top_ten/' + query + '/:fn.json?callback=:fn', callback);      
     }
 
     this.total = function(callback) {
-      this._request('/total/:callback.json?callback=:callback', callback);
+      this._request('/total/:fn.json?callback=:fn', callback);
     }
 
     this._request = function(path, callback) {
-      var functionName = 'everydayhero_' + Math.floor(Math.random()*1000001);    
+      var fn = 'everydayhero_' + Math.floor(Math.random()*1000001);    
       reqwest({
-        url: this._url(path.replace(/:callback/g, functionName)),
+        url: this._url(path.replace(/:fn/g, fn)),
         type: 'jsonp',
         jsonpCallback: 'callback',
         success: callback
